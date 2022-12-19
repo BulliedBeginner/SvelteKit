5b8820ffdf3c6dddaa90738c424d95f6f5aac6ae
HonWiCong <72854692+BulliedBeginner@users.noreply.github.com>
2022-12-19 15:23:09 +0800

    .

--- testing/src/hooks.server.ts
+++
@@ -1,5 +0,0 @@
-import { start_mongo } from "./database/mongo"
-
-start_mongo().then(() => {
-	console.log("Mongo Started")
-})
--- testing/src/routes/+page.ts
+++
@@ -1,9 +0,0 @@
-export const load = async () => {
-	const response = await fetch('https://dummyjson.com/products/search?q=phone');
-	const data = await response.json();
-	const products = data.products;
-	
-	return {
-		products : products
-	}
-}
